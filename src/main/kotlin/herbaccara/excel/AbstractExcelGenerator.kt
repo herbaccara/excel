0980fe282b043428d7fbb2214e05c90e410eb545
allbegray <allbegray@gmail.com>
2023-03-28 14:06:52 +0900

    :construction: SingleSheetExcelGenerator 추상화

---
+++ src/main/kotlin/herbaccara/excel/AbstractExcelGenerator.kt
@@ -0,0 +1,99 @@
+package herbaccara.excel
+
+import herbaccara.excel.annotation.ExcelStyle
+import org.apache.poi.common.usermodel.HyperlinkType
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.CellStyle
+import org.apache.poi.ss.usermodel.RichTextString
+import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.xssf.streaming.SXSSFWorkbook
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.OutputStream
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
+abstract class AbstractExcelGenerator<T>(excelType: ExcelType) : ExcelGenerator<T> {
+
+    companion object {
+        internal val DEFAULT_HEADER_STYLE = "${this::class.java.name}.DEFAULT_HEADER_STYLE"
+        internal val DEFAULT_BODY_STYLE = "${this::class.java.name}.DEFAULT_BODY_STYLE"
+    }
+
+    protected val workbook: Workbook = when (excelType) {
+        ExcelType.HSSF -> HSSFWorkbook()
+        ExcelType.XSSF -> XSSFWorkbook()
+        ExcelType.SXSSF -> SXSSFWorkbook()
+    }
+
+    protected abstract val styles: MutableMap<String, CellStyle>
+
+    protected fun createStyle(styleName: String, excelStyle: ExcelStyle) {
+        if (ExcelStyle.isDefault(excelStyle)) return
+
+        val font = workbook.createFont().apply {
+            if (excelStyle.fontName.isNotBlank()) {
+                fontName = excelStyle.fontName
+            }
+            fontHeight = excelStyle.fontHeight
+            color = excelStyle.fontColor.index
+            bold = excelStyle.fontBold
+            italic = excelStyle.fontItalic
+            strikeout = excelStyle.fontStrikeout
+            underline = excelStyle.fontUnderline.toByte()
+        }
+
+        val cellStyle = workbook.createCellStyle().apply {
+            setFont(font)
+            shrinkToFit = excelStyle.shrinkToFit
+            fillPattern = excelStyle.fillPattern
+            fillForegroundColor = excelStyle.fillForegroundColor.index
+            fillBackgroundColor = excelStyle.fillBackgroundColor.index
+            alignment = excelStyle.alignment
+            verticalAlignment = excelStyle.verticalAlignment
+
+            borderTop = excelStyle.borderStyle
+            borderLeft = excelStyle.borderStyle
+            borderRight = excelStyle.borderStyle
+            borderBottom = excelStyle.borderStyle
+
+            topBorderColor = excelStyle.borderColor.index
+            leftBorderColor = excelStyle.borderColor.index
+            rightBorderColor = excelStyle.borderColor.index
+            bottomBorderColor = excelStyle.borderColor.index
+        }
+
+        styles[styleName] = cellStyle
+    }
+
+    protected fun setCellValue(cell: Cell, value: Any?) {
+        when (value) {
+            is Number -> cell.setCellValue(value.toDouble())
+            is Boolean -> cell.setCellValue(value)
+            is LocalDateTime -> cell.setCellValue(value)
+            is LocalDate -> cell.setCellValue(value)
+            is Date -> cell.setCellValue(value)
+            is RichTextString -> cell.setCellValue(value)
+            else -> {
+                val str = value?.toString()?.ifBlank { "" } ?: ""
+                cell.setCellValue(str)
+                @Suppress("HttpUrlsUsage")
+                if (str.startsWith("http://") || str.startsWith("https://")) {
+                    val hyperlink = workbook.creationHelper.createHyperlink(HyperlinkType.URL).apply {
+                        address = str
+                    }
+                    cell.hyperlink = hyperlink
+                }
+            }
+        }
+    }
+
+    override fun write(os: OutputStream) {
+        os.use {
+            workbook.write(it)
+            workbook.close()
+            (workbook as? SXSSFWorkbook)?.dispose()
+        }
+    }
+}
--- src/main/kotlin/herbaccara/excel/SingleSheetExcelGenerator.kt
+++ src/main/kotlin/herbaccara/excel/SingleSheetExcelGenerator.kt
@@ -4,36 +4,18 @@ import herbaccara.excel.annotation.ExcelColumn
 import herbaccara.excel.annotation.ExcelSheet
 import herbaccara.excel.annotation.ExcelStyle
 import herbaccara.excel.annotation.Sort
-import org.apache.poi.common.usermodel.HyperlinkType
-import org.apache.poi.hssf.usermodel.HSSFWorkbook
-import org.apache.poi.ss.usermodel.*
-import org.apache.poi.xssf.streaming.SXSSFWorkbook
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import java.io.OutputStream
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.util.*
+import org.apache.poi.ss.usermodel.CellStyle
+import org.apache.poi.ss.usermodel.Sheet
 
 class SingleSheetExcelGenerator<T> @JvmOverloads constructor(
     clazz: Class<T>,
     excelType: ExcelType = ExcelType.SXSSF
-) : ExcelGenerator<T> {
-
-    companion object {
-        private val DEFAULT_HEADER_STYLE = "${this::class.java.name}.DEFAULT_HEADER_STYLE"
-        private val DEFAULT_BODY_STYLE = "${this::class.java.name}.DEFAULT_BODY_STYLE"
-    }
-
-    private val workbook: Workbook = when (excelType) {
-        ExcelType.HSSF -> HSSFWorkbook()
-        ExcelType.XSSF -> XSSFWorkbook()
-        ExcelType.SXSSF -> SXSSFWorkbook()
-    }
+) : AbstractExcelGenerator<T>(excelType) {
 
     private val sheet: Sheet
     private var currentRowIndex: Int = 0
     private val cellInfos: List<CellInfo>
-    private val styles: MutableMap<String, CellStyle> = mutableMapOf()
+    override val styles: MutableMap<String, CellStyle> = mutableMapOf()
 
     init {
         val excelSheet = clazz.getAnnotation(ExcelSheet::class.java) ?: throw IllegalArgumentException("")
@@ -78,66 +60,6 @@ class SingleSheetExcelGenerator<T> @JvmOverloads constructor(
         }
     }
 
-    private fun createStyle(styleName: String, excelStyle: ExcelStyle) {
-        if (ExcelStyle.isDefault(excelStyle)) return
-
-        val font = workbook.createFont().apply {
-            if (excelStyle.fontName.isNotBlank()) {
-                fontName = excelStyle.fontName
-            }
-            fontHeight = excelStyle.fontHeight
-            color = excelStyle.fontColor.index
-            bold = excelStyle.fontBold
-            italic = excelStyle.fontItalic
-            strikeout = excelStyle.fontStrikeout
-            underline = excelStyle.fontUnderline.toByte()
-        }
-
-        val cellStyle = workbook.createCellStyle().apply {
-            setFont(font)
-            shrinkToFit = excelStyle.shrinkToFit
-            fillPattern = excelStyle.fillPattern
-            fillForegroundColor = excelStyle.fillForegroundColor.index
-            fillBackgroundColor = excelStyle.fillBackgroundColor.index
-            alignment = excelStyle.alignment
-            verticalAlignment = excelStyle.verticalAlignment
-
-            borderTop = excelStyle.borderStyle
-            borderLeft = excelStyle.borderStyle
-            borderRight = excelStyle.borderStyle
-            borderBottom = excelStyle.borderStyle
-
-            topBorderColor = excelStyle.borderColor.index
-            leftBorderColor = excelStyle.borderColor.index
-            rightBorderColor = excelStyle.borderColor.index
-            bottomBorderColor = excelStyle.borderColor.index
-        }
-
-        styles[styleName] = cellStyle
-    }
-
-    protected fun setCellValue(cell: Cell, value: Any?) {
-        when (value) {
-            is Number -> cell.setCellValue(value.toDouble())
-            is Boolean -> cell.setCellValue(value)
-            is LocalDateTime -> cell.setCellValue(value)
-            is LocalDate -> cell.setCellValue(value)
-            is Date -> cell.setCellValue(value)
-            is RichTextString -> cell.setCellValue(value)
-            else -> {
-                val str = value?.toString()?.ifBlank { "" } ?: ""
-                cell.setCellValue(str)
-                @Suppress("HttpUrlsUsage")
-                if (str.startsWith("http://") || str.startsWith("https://")) {
-                    val hyperlink = workbook.creationHelper.createHyperlink(HyperlinkType.URL).apply {
-                        address = str
-                    }
-                    cell.hyperlink = hyperlink
-                }
-            }
-        }
-    }
-
     override fun addRows(items: List<T>) {
         items.forEach { item ->
             val row = sheet.createRow(currentRowIndex++)
@@ -147,16 +69,6 @@ class SingleSheetExcelGenerator<T> @JvmOverloads constructor(
                 }
                 val value = cellInfo.field.get(item)
                 setCellValue(cell, value)
-            }
-        }
-    }
-
-    override fun write(os: OutputStream) {
-        os.use {
-            workbook.write(it)
-            workbook.close()
-            if (workbook is SXSSFWorkbook) {
-                workbook.dispose()
             }
         }
     }
